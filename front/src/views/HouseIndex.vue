a567ba580031f6de157de257717636d0eceaf724
Wise Programmer <67681207+gandi0330@users.noreply.github.com>
2022-05-13 16:35:50 +0900

    style : 메인페이지 이미지 추가

--- front/src/views/HouseIndex.vue
+++ front/src/views/HouseIndex.vue
@@ -1,27 +1,26 @@
 <template>
   <div>
     <section>
-      <!-- start main image! lime your light -->
       <div class="container">
-        <div class="blank"></div>
-        <div class="main_img">
-          <!-- <a href="./apt.jsp"
-            ><img src="./img/house_1.png" id="mainImage" width="960px" alt=""
-          /></a> -->
+        <div></div>
+        <div>
+          <a href="#"
+            ><img src="@/assets/house_1.png" width="960px" alt=""
+          /></a>
         </div>
-        <div class="sub-title-left">
-          <p id="title">HAPPYHOUSE</p>
-          <p id="sub-title">
+        <div>
+          <p>HAPPYHOUSE</p>
+          <p>
             주택의 아파트/다세대, 주택 별 거래 정보와 아파트 정보 제공 사이트
           </p>
         </div>
-        <div class="sub-title-right"><p id="sub-title">CLICK</p></div>
+        <div><p>CLICK</p></div>
       </div>
       <div v-if="aptlist">
-        <div class="apthot">
-          <p id="title">인기 관심 매물 순위</p>
-          <div class="apthot">
-            <table id="apthot">
+        <div>
+          <p>인기 관심 매물 순위</p>
+          <div>
+            <table>
               <colgroup>
                 <col width="100" />
                 <col width="300" />
@@ -29,14 +28,16 @@
                 <col width="120" />
               </colgroup>
               <thead>
-                <tr class="tr_size">
-                  <th class="apt_title_main">순위</th>
-                  <th class="apt_title_main">아파트이름</th>
-                  <th class="apt_title_main">주소</th>
-                  <th class="apt_title_main">건축연도</th>
+                <tr>
+                  <thead>
+                    순위
+                  </thead>
+                  <th>아파트이름</th>
+                  <th>주소</th>
+                  <th>건축연도</th>
                 </tr>
               </thead>
-              <tbody id="apthotlist">
+              <tbody>
                 <div v-for="(apt, index) in items" :key="index">
                   <tr>
                     <td>${index + 1}</td>
@@ -50,7 +51,7 @@
           </div>
         </div>
       </div>
-      <div class="long-line"><hr /></div>
+      <div><hr /></div>
     </section>
   </div>
 </template>
